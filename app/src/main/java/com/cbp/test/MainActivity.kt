064530b68b1300f19f10dc0ce8bf02bd19ea17a3
Jordan Tuffery <tufferyjordan@gmail.com>
2020-05-29 15:17:50 +0200

    moved res in design, create toolbar and menu interactable

--- app/src/main/java/com/cbp/test/MainActivity.kt
+++ app/src/main/java/com/cbp/test/MainActivity.kt
@@ -2,11 +2,33 @@ package com.cbp.test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(main_activity_toolbar)
+
+        main_activity_toolbar.setOnMenuItemClickListener {
+            when(it.itemId) {
+                R.id.menu_item_barcode -> {
+                    Toast.makeText(this, "Barcode requested", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
+        return true
     }
 }
